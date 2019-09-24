890031d0fc28c69996008e604e109903d653ed6d
chenbingyu <614263154@qq.com>
2019-09-24 11:47:24 +0800

    删除 添加 修改

--- 初始代码/web_back/utils/category.js
+++ 初始代码/web_back/utils/category.js
@@ -2,29 +2,43 @@
  * 文章类别
  */
 
- var category = {
+var category = {
     //显示获取
-   show: function(callBack){
-    //    $.ajax({
-    //        url: 'http://localhost:8000/admin/category_search',
-    //        type: 'get',
-    //        success: function(res){
-    //            callBack(res);
-    //        }
-    //    });
-          $.get(URLIST.category_show,function(res){
-              callBack(res);
-          });
-   },
+    show: function (callBack) {
+
+        //    $.ajax({
+        //        url: 'http://localhost:8000/admin/category_search',
+        //        type: 'get',
+        //        success: function(res){
+        //            callBack(res);
+        //        }
+        //    });
+        $.get(URLIST.category_show, function (res) {
+            callBack(res);
+        });
+    },
     //删除
-    add: function(name, slug, callBack){
-        $.post(URLIST.category_add, { name:name,slug:slug}, function(res){
-              callBack(res);
-          });
+    del: function (id, callBack) {
+        $.post(URLIST.category_delete, { id: id }, function (res) {
+            callBack(res);
+        });
+    },
+
+    //添加
+    add: function (name, slug, callBack) {
+        $.post(URLIST.category_add, { name: name, slug: slug }, function (res) {
+            callBack(res);
+        });
+    },
+
+
+    //编辑
+    edit: function (id, name, slug, callBack) {
+        $.post(URLIST.category_edit, { name: name, id: id, slug: slug }, function (res) {
+            callBack(res);
+        })
     }
 
 
 
-
-
- }
+}
--- 初始代码/web_back/utils/config.js
+++ 初始代码/web_back/utils/config.js
@@ -6,4 +6,6 @@ var URLIST = {
 
   category_show: BASE_URL + '/admin/category_search',
   category_add: BASE_URL + '/admin/category_add',
+  category_delete: BASE_URL + '/admin/category_delete',
+  category_edit: BASE_URL + '/admin/category_edit',
 };
